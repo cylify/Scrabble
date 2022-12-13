599b2e0f9112b62d3eb3d81ea9031a3da0041c8b
cylify <mradi.lachman@gmail.com>
2022-12-13 12:48:02 -0500

    Added new methods and completed the method to get the score for each letter

--- Letter.java
+++ Letter.java
@@ -1,13 +1,58 @@
 class Letter {
-	HashMap<String, Integer> letterMap;
+	private char letter;
+	private int val;
 
-	public Letter() {
-		this.letterMap = new HashMap<>();
-	} 
+	public Letter(char letter, int val) {
+		this.val = val;
+		this.letter = letter;
+	}
 
-	public int getLetterScore() {
-		
+	public getLetter() {
+		return this.letter;
+	}
+
+	public getVal() {
+		return this.val;
+	}
+
+	public void setLetter(char letter) {
+		this.letter = letter;
+	}
+
+	public void getVal(int val) {
+		this.val = val;
 	}
 
 
+	public HashMap<Character, Integer> getLetterScore() {
+		HashMap<Character, Integer> letterScore = new HashMap<>() {{
+			put('A', 1);
+			put('E', 1);
+			put('I', 1);
+			put('O', 1);
+			put('U', 1);
+			put('L', 1);
+			put('N', 1);
+			put('S', 1);
+			put('T', 1);
+			put('R', 1);
+			put('D', 2);
+			put('G', 2);
+			put('B', 3);
+			put('C', 3);
+			put('M', 3);
+			put('P', 3);
+			put('F', 4);
+			put('H', 4);
+			put('V', 4);
+			put('W', 4);
+			put('Y', 4);
+			put('K', 5);
+			put('J', 8);
+			put('X', 8);
+			put('Q', 10);
+			put('Z', 10);
+		}};
+		return letterAndScore;
+	}
 }
